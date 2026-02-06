b564b62f62e461cc33e5403305d70ac829994567
Ekenma Peter <ekenma.peter@gmail.com>
2026-02-06 09:39:39 +0100

    Fix path logic in firebase config

--- config/firebase.php
+++ config/firebase.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'credentials' => env('FIREBASE_CREDENTIALS', storage_path('app/firebase-credentials.json')),
+    'credentials' => env('FIREBASE_CREDENTIALS', 'storage/app/firebase-credentials.json'),
 ];
